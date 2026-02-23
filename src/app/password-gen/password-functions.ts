82424e3610c371c5024afdb3223f4f26c4977cba
Ronan Hanley <ronan.h64@gmail.com>
2026-02-23 23:37:57 +0000

    Add password gen fns and basic page layout

--- src/app/password-gen/password-functions.ts
+++ src/app/password-gen/password-functions.ts
@@ -1,13 +1,56 @@
 
-type PasswordOptions = {
-    numWords: number;
-    capitalFirstChar: boolean;
-    includeDigit: boolean;
-    includeSpecial: boolean;
-    minLength: number;
-    maxLength: number;
+const NO_SHIFT_SYMBOLS = ['-','=','[',']','\\',';',"'",',','.','/'];
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function pickRandomFromArray<T>(array: Array<T>): T {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
 }
 
-function generatePassword(options: PasswordOptions) {
+function insertStrAtIndex(str: string, c: string, index: number) {
+    return [str.slice(0, index), c, str.slice(index)].join('');
+}
+
+type PasswordOptions = {
+    numWords: number;
+    capitalEnd: boolean;
+    includeSymbol: boolean;
+    includeDigit: boolean;
+}
+
+export function generatePassword(wordlist: string[], options: PasswordOptions): string {
+    let password = '';
     
+    for (let i = 0; i < options.numWords; i++) {
+        const nextIndex = Math.floor(Math.random() * wordlist.length);
+        password += wordlist[nextIndex];
+    }
+
+    if (options.includeDigit) {
+        const digit = pickRandomFromArray(DIGITS);
+        const index = Math.floor(Math.random() * (password.length - 1)) + 1;
+        password = insertStrAtIndex(password, digit, index);
+    }
+
+    if (options.includeSymbol) {
+        const symbol = pickRandomFromArray(NO_SHIFT_SYMBOLS);
+        const index = Math.floor(Math.random() * (password.length - 1)) + 1;
+        password = insertStrAtIndex(password, symbol, index);
+    }
+
+    return password;
+}
+
+export function generatePasswords(wordlist: string[], options: PasswordOptions, num: number) {
+    const passwords = [];
+
+    for (let i = 0; i < num; i++) {
+        const password = generatePassword(wordlist, options);
+        passwords.push({
+            key: i,
+            password,
+        });
+    }
+
+    return passwords;
 }
--- src/app/password-gen/password-gen.tsx
+++ src/app/password-gen/password-gen.tsx
@@ -1,5 +1,65 @@
 'use client'
 
+import { useState } from "react";
+import { WORD_LIST } from "./words";
+import { generatePasswords } from "./password-functions";
+import CopyButton from "../components/copy-button";
+import React from "react";
+import { Slider } from "@/components/ui/slider";
+import { Controller, useForm } from "react-hook-form";
+import { Label } from "@/components/ui/label";
+
+const NUM_GEN = 10;
+
 export default function PasswordGen() {
-    return null;
+    const { control, watch } = useForm({
+        defaultValues: {
+            numWords: [2],
+        }
+    });
+
+    const [passwords, setPasswords] = useState(generatePasswords(
+        WORD_LIST,
+        {
+            numWords: 1,
+            capitalEnd: true,
+            includeDigit: true,
+            includeSymbol: true,
+        },
+        NUM_GEN,
+    ));
+
+    return (<>
+        <div className="mx-auto grid w-full max-w-xs gap-3">
+            <div className="flex items-center justify-between gap-2">
+                <Label htmlFor="slider-demo-temperature">Num words</Label>
+                <span className="text-muted-foreground text-sm">
+                {watch('numWords')}
+                </span>
+            </div>
+
+            <Controller
+                control={control}
+                name="numWords"
+                render={({ field }) => (
+                    <Slider
+                    min={2}
+                    max={5}
+                    step={1}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    />
+                )}
+                />
+        </div>
+        <div className="grid grid-cols-2 grid-cols-[auto_auto] gap-3">
+            {passwords.map(({ key, password }) => (
+                <React.Fragment key={key}>
+                    <span className="text-xl">{password}</span>
+                    <CopyButton content={password} />
+                </React.Fragment>
+            ))}
+        </div>
+        </>
+    );
 }
