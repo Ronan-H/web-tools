d77842bf581110447d3814b3048d76fe6f5e7c76
Ronan Hanley <ronan.h64@gmail.com>
2026-02-25 21:14:39 +0000

    Improve layout and trim word list

--- src/app/password-gen/password-gen.tsx
+++ src/app/password-gen/password-gen.tsx
@@ -64,15 +64,15 @@ export default function PasswordGen() {
     const fontSizeMap = {
         1: 'text-4xl',
         2: 'text-3xl',
-        3: 'text-3xl',
-        4: 'text-3xl',
+        3: 'text-2xl',
+        4: 'text-xl',
     } as { [key: number]: string };
     const fontSize = fontSizeMap[numWords[0]];
 
     // This is to avoid hydration mismatch errors, and avoid calculating passwords server-side
     const passwordPlaceholder = () => (
         <div className="flex flex-col items-center">
-            <span className={`${fontSize} whitespace-nowrap invisible`}>-</span>
+            <span className={`${fontSize} break-all font-bold invisible`}>-</span>
             <span className="italic invisible">
                 -
             </span>
@@ -83,7 +83,7 @@ export default function PasswordGen() {
     const passwordSection = () => (
         <div className="grid grid-cols-2 grid-cols-[auto_auto] gap-3">
             <div className="flex flex-col items-center">
-                <span className={`${fontSize} whitespace-nowrap`}>{password.password}</span>
+                <span className={`${fontSize} break-all font-bold`}>{password.password}</span>
                 <span className="italic">
                     {password.words.join(', ')}
                 </span>
@@ -92,59 +92,62 @@ export default function PasswordGen() {
         </div>
     );
 
-    return (<>
-        <div className="flex flex-col items-center gap-3 w-50">
-            <FieldGroup className="max-w-sm mb-3">
-                <Field>
-                    <Label htmlFor="numWords">Number of Words</Label>
-                    <FieldDescription>{numWords}</FieldDescription>
-                    <Controller
-                        control={control}
-                        name="numWords"
-                        render={({ field }) => (
-                            <Slider
-                                min={1}
-                                max={4}
-                                step={1}
-                                value={field.value}
-                                onValueChange={field.onChange}
-                            />
-                        )}
-                    />
-                </Field>
-                <div className="grid grid-cols-[1fr_auto] gap-x-6 gap-y-3 items-center">
-                    {CHECKBOX_FIELDS.map(({ fieldName, label }) => (
-                        <React.Fragment key={fieldName}>
-                            <Label htmlFor={fieldName}>{label}</Label>
+    return (
+        <div className="flex flex-col items-center gap-3">
+            <div className="flex flex-col items-center gap-3 w-50">
+                <FieldGroup className="max-w-sm mb-3">
+                    <Field>
+                        <Label htmlFor="numWords">Number of Words</Label>
+                        <FieldDescription>{numWords}</FieldDescription>
+                        <Controller
+                            control={control}
+                            name="numWords"
+                            render={({ field }) => (
+                                <Slider
+                                    min={1}
+                                    max={4}
+                                    step={1}
+                                    value={field.value}
+                                    onValueChange={field.onChange}
+                                />
+                            )}
+                        />
+                    </Field>
+                    <div className="grid grid-cols-[1fr_auto] gap-x-6 gap-y-3 items-center">
+                        {CHECKBOX_FIELDS.map(({ fieldName, label }) => (
+                            <React.Fragment key={fieldName}>
+                                <Label htmlFor={fieldName}>{label}</Label>
 
-                            <Controller
-                                name={fieldName}
-                                control={control}
-                                render={({ field }) => (
-                                    <Checkbox
-                                        id={fieldName}
-                                        checked={field.value}
-                                        onCheckedChange={field.onChange}
-                                    />
-                                )}
-                            />
-                        </React.Fragment>
-                    ))}
+                                <Controller
+                                    name={fieldName}
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Checkbox
+                                            id={fieldName}
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                        />
+                                    )}
+                                />
+                            </React.Fragment>
+                        ))}
+                    </div>
+                </FieldGroup>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+                <span className="text-sm text-muted-foreground">Generated password:</span>
+                {isClient ? passwordSection() : passwordPlaceholder()}
+                <div className="flex flex-row gap-3">
+                    <CopyButton content={password.password} />
+                    <Button
+                        variant="outline"
+                        className={"cursor-pointer aspect-square"}
+                        onClick={() => setIteration((i) => i + 1)}
+                    >
+                        <RefreshCw />
+                    </Button>
                 </div>
-            </FieldGroup>
-            <span className="text-sm text-muted-foreground">Generated password:</span>
-            {isClient ? passwordSection() : passwordPlaceholder()}
-            <div className="flex flex-row">
-                <CopyButton content={password.password} />
-                <Button
-                    variant="outline"
-                    className={"cursor-pointer aspect-square"}
-                    onClick={() => setIteration((i) => i + 1)}
-                >
-                    <RefreshCw />
-                </Button>
             </div>
         </div>
-    </>
     );
 }
